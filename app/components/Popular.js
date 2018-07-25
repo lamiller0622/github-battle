9ff8e1f385d5f821f1d3e98df95e4e8373a29299
Luke Miller <lamiller0622@gmail.com>
2018-07-25 10:21:28 -0400

    Update Popular.js

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,7 +1,7 @@
-const React = require('react');
-const PropTypes = require('prop-types');
-const api = require('../utils/api');
-const Loading = require('./Loading');
+import React from 'react';
+import PropTypes from 'prop-types';
+import api from '../utils/api';
+import Loading from './Loading';
 
 function SelectLanguage ({ selectedLanguage, onSelect }) {
     const languages = ['All','Javascripts','Ruby','Java','CSS','Python'];
@@ -92,4 +92,4 @@ class Popular extends React.Component {
     }
 }
 
-module.exports = Popular;
+export default Popular;
